b616378576430b784fec4c50f6f0d0838503eb6f
Charles Severance <csev@umich.edu>
2020-07-27 12:27:20 -0400

    Move the score bits to the right place

--- json/content_json_messages.php
+++ json/content_json_messages.php
@@ -4,41 +4,32 @@ function getLtiLinkJSON($url) {
 
 // https://www.imsglobal.org/specs/lticiv1p0/specification
 $return = '{
-  "@context" : "http://purl.imsglobal.org/ctx/lti/v1/ContentItem", 
-  "@graph" : [ 
+  "@context" : [
+    "http://purl.imsglobal.org/ctx/lti/v1/ContentItem",
+    {
+      "lineItem" : "http://purl.imsglobal.org/ctx/lis/v2/LineItem",
+      "res" : "http://purl.imsglobal.org/ctx/lis/v2p1/Result#"
+    }
+  ],
+  "@graph" : [
     { "@type" : "LtiLinkItem",
-      "@id" : ":item2",
-      "text" : "The mascot for the Sakai Project", 
-      "title" : "The fearsome mascot of the Sakai Project",
-      "url" : "http://developers.imsglobal.org/images/imscertifiedsm.png",
-      "icon" : {
-        "@id" : "fa-bullseye",
-        "width" : 50,
-        "height" : 50
-      },
-      "submission" : {
-          "startDatetime" : "2016-11-07T00:00:00Z",
-          "endDatetime" : "2016-12-01T00:00:00Z"
-      },
-      "available" : {
-        "startDatetime" : "2016-10-31T19:20:30Z",
-        "endDatetime" : "2016-12-01T00:00:00Z"
-      },
+      "mediaType" : "application/vnd.ims.lti.v1.ltilink",
+      "title" : "Chapter 12 quiz",
       "lineItem" : {
-         "@type" : "LineItem",
-          "label" : "The mascot for the Sakai Project",
-          "reportingMethod" : "res:totalScore",
-           "assignedActivity" : {
-                "@id" : "http://www.tsugi.org/assessment/66400",
-                "activity_id" : "a-9334df-33"
-            }
+        "@type" : "LineItem",
+        "label" : "Chapter 12 quiz",
+        "reportingMethod" : "res:totalScore",
+        "assignedActivity" : {
+          "@id" : "http://toolprovider.example.com/assessment/66400",
+          "activity_id" : "a-9334df-33"
         },
         "scoreConstraints" : {
-            "@type" : "NumericLimits",
-            "normalMaximum" : 100,
-            "extraCreditMaximum" : 10,
-            "totalMaximum" : 110
+          "@type" : "NumericLimits",
+          "normalMaximum" : 100,
+          "extraCreditMaximum" : 10,
+          "totalMaximum" : 110
         }
+      }
     }
   ]
 }';
